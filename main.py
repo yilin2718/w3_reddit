7b979085ed1bac29f55b02da4477e24a3cf44cf6
yilin2718 <yilin2.178@gmail.com>
2022-04-30 10:56:50 -0500

    made changes in Main.py by yi

--- main.py
+++ main.py
@@ -3,53 +3,25 @@ from typing import Optional
 from pydantic import BaseModel
 #https://pydantic-docs.helpmanual.io/usage/models/
 
+
+
 app = FastAPI()
 @app.get("/")
 def hello_world():
-    return {"Hello": "World"}
+    return {"Hello": "yi"}
 
-
-"""
-Ml part 
-"""
-import praw
 from transformers import pipeline
-from transformers import pipeline
-import random
-
-
-reddit = praw.Reddit(
-    client_id=config.REDDIT_API_CLIENT_ID,
-    client_secret=config.REDDIT_API_CLIENT_SECRET,
-    user_agent=config.REDDIT_API_USER_AGENT,
-)
-
-subreddit = reddit.subreddit('TSLA') 
-
-from praw.models import MoreComments
-
-top_comments = []
-
-for submission in subreddit.top(limit=10):
-    for top_level_comment in submission.comments:
-        if isinstance(top_level_comment, MoreComments):
-                    continue
-        top_comments.append(top_level_comment.body)
+import config
 
 sentiment_model = pipeline("sentiment-analysis")
-def get_random_comment(conversations):
-    comment = random.choice(conversations)
-    return comment
-
-
 
 class PredictionRequest(BaseModel):
   query_string: str
+  #query_string_list: List[str]  
 
-@app.post("/sentiment-sum")
-async def calculate_sentiment_sum(predictionrequest: PredictionRequest):
-    return predictionrequest
-
-
+@app.post("/new-sentiment")
+async def analyze_sentiment(username: str, text: str):    
+    user_inputted_sentiment = sentiment_model(text)
+    return {f"USER: {username} TEXT: {text} SENTIMENT: {user_inputted_sentiment}"}
 
 
